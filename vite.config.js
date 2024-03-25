6b4db4503df5dad288b70a2833f7c5e0631ccb9d
CristianVelez <cristiansep0293@gmail.com>
2024-03-25 13:25:49 -0500

    Primer commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "https://cristiansep0293.github.io/random-interface-react",
+  server: {
+    port: 5500,
+    https: false,
+    host: "127.0.0.1",
+  },
+});
